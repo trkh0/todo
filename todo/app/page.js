a1f3a42ecc039298939c1de5ba216a12843502b5
trkh0 <trk.huni818@gmail.com>
2024-07-27 11:24:06 +0300

    feature: supabase connection and test

--- todo/app/page.js
+++ todo/app/page.js
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import Navbar from "./components/navbar/Navbar.js";
+import { getSupabaseClient, supabaseClient } from "./utils/supabase/SupabaseClient.js";
 
-export default function Home() {
+export const revalidate = 1;
+
+export default async function Home() {
+  let supabase = getSupabaseClient();
+  let { data: zodiacsigns } = await supabase
+  .from("test")
+  .select("*")
+  console.log(zodiacsigns);
+
+
   return (
     <div className="tw-bg-white">
       <Navbar />
+
     </div>
   );
 }
